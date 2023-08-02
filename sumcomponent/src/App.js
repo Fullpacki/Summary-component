ca8fde639a8da5642d093e8ad872c0fbf5bef25f
Fullpacki <113352776+Fullpacki@users.noreply.github.com>
2023-08-02 15:59:01 +0200

    edit

--- sumcomponent/src/App.js
+++ sumcomponent/src/App.js
@@ -4,7 +4,7 @@ import Summary from "./components/Container";
 
 function App() {
   return (
-    <div className="App flex flex-col max-w-2xl mx-auto xl:bg-white xl:rounded-xl xl:flex-row xl:w-full">
+    <div className="App flex flex-col max-w-2xl mx-auto xl:bg-white xl:rounded-xl xl:flex-row xl:w-full xl:shadow-2xl xl:shadow-[#4c37f4]/30">
       <Results />
       <div className="flex flex-col xl:w-1/2 xl:rounded-xl  xl:py-4 ">
         <Summary />
--- sumcomponent/src/components/Header.js
+++ sumcomponent/src/components/Header.js
@@ -3,12 +3,12 @@ function Results() {
     <div className="text-center bg-gradient-to-b from-[#6843ff] to-[#312ce9]  rounded-b-3xl text-white flex flex-col justify-center items-center px-6 pb-10 py-6 xl:w-1/2 xl:rounded-xl">
       <header className="flex flex-col gap-5 w-full justify-center items-center">
         <h1 className="font-bold text-[#cac9ff]">Your Result</h1>
-        <div className="w-36 h-36 flex flex-col items-center justify-center rounded-full bg-gradient-to-b from-[#4c22cb] to-[#4635f0]">
-          <div className="font-extrabold text-6xl">76</div>
+        <div className="w-36 h-36 xl:w-48 xl:h-48 xl:w- flex flex-col items-center justify-center rounded-full bg-gradient-to-b from-[#4c22cb] to-[#4635f0]">
+          <div className="font-extrabold text-6xl xl:text-7xl">76</div>
           <div className="font-bold text-[#7872f5]">of 100</div>
         </div>
-        <h2 className="text-2xl font-bold">Great</h2>
-        <p className="text-[#cac9ff] w-4/5">
+        <h2 className="text-2xl xl:text-3xl font-bold">Great</h2>
+        <p className="text-[#cac9ff] xl:text-lg xl:w-5/6 w-4/5">
           Your performance exeed 65% of the people conductiong the test here!
         </p>
       </header>
